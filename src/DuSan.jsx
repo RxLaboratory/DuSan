3817202946e63eb5d45b70e51afc4aff02cb2e68
Duduf <git@duduf.com>
2020-11-14 18:28:36 +0100

    icons

--- src/DuSan.jsx
+++ src/DuSan.jsx
@@ -3,62 +3,72 @@
 //TODO
 //polices manquantes
 
+//icons
+var im = DuAEF.DuBinary.toFile(w25_brain_m);
+var ib = DuAEF.DuBinary.toFile(w25_brain_b);
+var iy = DuAEF.DuBinary.toFile(w25_brain_y);
+var io = DuAEF.DuBinary.toFile(w25_brain_o);
+var ir = DuAEF.DuBinary.toFile(w25_brain_r);
+var ip = DuAEF.DuBinary.toFile(w25_brain_p);
+
 // Main Functions
 function checkSanity()
 {
     var level = DuAEF.DuSanity.currentLevel;
     if (level == DuAEF.DuSanity.Levels.UNKNOWN)
     {
-        ui_sanityButton.label.text = "Sanity level: unknown";
-        ui_sanityButton.setBackgroundColor(DuAEF.DuJS.Color.Colors.AE_DARK_GREY);
-        ui_sanityButton.setTextColor(DuAEF.DuJS.Color.Colors.LIGHT_GREY);
+        ui_sanityButton.setStandardImage(im);
+        ui_sanityButton.label.text = "Unknown";
     }
     else if (level == DuAEF.DuSanity.Levels.OK)
     {
-        ui_sanityButton.label.text = "Sanity level: OK";
-        ui_sanityButton.setBackgroundColor(DuAEF.DuJS.Color.Colors.VERY_DARK_GREY);
-        ui_sanityButton.setTextColor(DuAEF.DuJS.Color.Colors.LIGHT_GREY);
+        ui_sanityButton.setStandardImage(im);
+        ui_sanityButton.label.text = "OK";
     }
     else if (level == DuAEF.DuSanity.Levels.INFO)
     {
-        ui_sanityButton.label.text = "Sanity level: INFO";
-        ui_sanityButton.setBackgroundColor(DuAEF.DuJS.Color.Colors.VERY_DARK_GREY);
-        ui_sanityButton.setTextColor(DuAEF.DuJS.Color.Colors.AFTER_EFFECTS_BLUE);
+        ui_sanityButton.setStandardImage(ib);
+        ui_sanityButton.label.text = "Info";
     }
     else if (level == DuAEF.DuSanity.Levels.WARNING)
     {
-        ui_sanityButton.label.text = "Sanity level: WARNING";
-        ui_sanityButton.setBackgroundColor(DuAEF.DuJS.Color.Colors.YELLOW);
-        ui_sanityButton.setTextColor(DuAEF.DuJS.Color.Colors.VERY_DARK_GREY);
+        ui_sanityButton.setStandardImage(iy);
+        ui_sanityButton.label.text = "WARNING";
     }
     else if (level == DuAEF.DuSanity.Levels.BAD)
     {
-        ui_sanityButton.label.text = "Sanity level: BAD";
-        ui_sanityButton.setBackgroundColor(DuAEF.DuJS.Color.Colors.ORANGE);
-        ui_sanityButton.setTextColor(DuAEF.DuJS.Color.Colors.BLACK);
+        ui_sanityButton.setStandardImage(io);
+        ui_sanityButton.label.text = "BAD";
     }
     else if (level == DuAEF.DuSanity.Levels.CRITICAL)
     {
-        ui_sanityButton.label.text = "Sanity level: CRITICAL";
-        ui_sanityButton.setBackgroundColor(DuAEF.DuJS.Color.Colors.RAINBOX_RED);
-        ui_sanityButton.setTextColor(DuAEF.DuJS.Color.Colors.WHITE);
+        ui_sanityButton.setStandardImage(ir);
+        ui_sanityButton.label.text = "CRITICAL";
     }
     else if (level == DuAEF.DuSanity.Levels.FATAL)
     {
-        ui_sanityButton.label.text = "Sanity level: FATAL";
-        ui_sanityButton.setBackgroundColor(DuAEF.DuJS.Color.Colors.LIGHT_PURPLE);
-        ui_sanityButton.setTextColor(DuAEF.DuJS.Color.Colors.WHITE);
+        ui_sanityButton.setStandardImage(ip);
+        ui_sanityButton.label.text = "FATAL";
     }
 }
 
 //UI
-var ui_sanityButton = DuAEF.DuScriptUI.addButton(ui.mainGroup, "Sanity level: unknown-------------------------------------");
-ui_sanityButton.onClick = function () {
-    ui.mainGroup.visible = false;
-    ui.sanityGroup.visible = true;
-}
+var ui_sanityButton = DuAEF.DuScriptUI.addButton(
+    ui.mainGroup,
+    "Unknown",
+    im,
+    "",
+    ir
+    );
+
+ui_sanityButton.label.minimumSize = [50,0];
+ui.sanityGroup.tieTo(ui_sanityButton);
+/*ui_sanityButton.onClick = function () {
+    ui.sanityGroup.visible = !ui.sanityGroup.visible;
+}*/
 
 DuAEF.DuScriptUI.addEvent(checkSanity);
+checkSanity();
 
 
 #include 'dusan_required/dusan_footer.jsxinc'
